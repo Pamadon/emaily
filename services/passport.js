16acdebebe75b01777311c513f6aad956d44870d
Peter Amadon <peteramadon@Peters-MacBook-Pro.local>
2018-01-11 13:56:46 -0800

    added routes and separate files for passport and mango model

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model("users");
 
 passport.use(new GoogleStrategy({
 	clientID: keys.googleClientID,
@@ -8,9 +11,18 @@ passport.use(new GoogleStrategy({
 	callbackURL: '/auth/google/callback'
 	},
 		(accessToken, refreshToken, profile, done) => {
-			console.log('accessToken', accessToken);
-			console.log('refreshToken', refreshToken);
-			console.log('profile', profile);
+			User.findOne({ googleId: profile.id})
+				.then((existingUser) => {
+					if (existingUser) {
+						// we already have a user with the given profile id
+						done(null, existingUser);
+					} else {
+						// we dont have a user make a new record
+						new User ({ googleId: profile.id })
+							.save()
+							.then(user => done(null, user));
+					}
+				});
 		}
 	)
 );
